9cbd757847a06cb9ba07697dfe42b963d702bf37
Wojtek <wojciech@mindsandmachines.com>
2015-10-03 23:11:06 +0100

    Init commit.

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+  "name": "taupo",
+  "private": true,
+  "version": "0.0.1",
+  "description": "Node.js application to trace our trip to New Zealand",
+  "author": "Wojtek Padula <wojtek@padula.me>",
+  "dependencies": {
+    "express": "3.2.4"
+  }
+}
